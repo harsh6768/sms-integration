98aad15f7fd21d0f5f8df456a61147e1a78e1c9c
harsh6768 <harshchaurasiya6768@gmail.com>
2020-02-17 11:14:13 +0530

    msg91 service added to send otp

--- routes/main-routes.js
+++ routes/main-routes.js
@@ -1,9 +1,17 @@
 const Router=require('express').Router();
 
 const twilioController=require('../controllers/twilio-controller');
+const msg91Controller=require('../controllers/msg91-controller');
 
+//twilio
 Router.route('/get_twilio_keys').get(twilioController.getTwilioKeys);
 Router.route('/send_sms_by_twilio').post(twilioController.sendSmsByTwilio);
+
+//msg91
+Router.route('/get_msg91_keys').get(msg91Controller.getMsg91Keys);
+Router.route('/send_otp_by_msg91').post(msg91Controller.sendOtpByMSG91);
+Router.route('/retry_send_otp_by_msg91').post(msg91Controller.retrySendOtpByMsg91);
+Router.route('/verify_otp_by_msg91').post(msg91Controller.verifyOtpByMsg91);
 
 
 module.exports=Router;
